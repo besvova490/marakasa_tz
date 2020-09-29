880dc5a41a58ab4b375dc7dc9c491bebf0e564c7
besvova490 <besvova490@gmail.com>
2020-09-29 22:14:24 +0300

    update requirements

--- pars_and_save.py
+++ pars_and_save.py
@@ -20,31 +20,31 @@ def open_reviews(file_name):
     return csv_reviews_list
 
 
-def add_product_to_db(product_dict):
+def add_product_to_db(products_list):
     try:
-        product_objects = []
-        for product in product_dict:
-            product_objects.append(
+        product_obj_list = []
+        for product in products_list:
+            product_obj_list.append(
                 Product(asin=product['Asin'], title=product['Title']))
-        db.session.bulk_save_objects(product_objects)
+        db.session.bulk_save_objects(product_obj_list)
         db.session.commit()
         return {'msg': 'All products added'}
     except IntegrityError:
         return {'msg': 'some product from file already in db'}
 
 
-def add_reviews_to_db(reviews_dict):
+def add_reviews_to_db(reviews_list):
     try:
-        reviews_list = []
-        for review in reviews_dict:
+        reviews_obj_list = []
+        for review in reviews_list:
             product_review = Product.query.filter_by(
                 asin=review['Asin']).first()
-            reviews_list.append(
+            reviews_obj_list.append(
                 Review(asin=review['Asin'], title=review['Title'],
                        review=review['Review'], product=product_review,
                        product_id=product_review.id)
             )
-        db.session.bulk_save_objects(reviews_list)
+        db.session.bulk_save_objects(reviews_obj_list)
         db.session.commit()
         return {'msg': 'All reviews added'}
     except IntegrityError:
